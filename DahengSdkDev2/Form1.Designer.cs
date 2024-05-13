d6a7a474248432862e692407294536867e72e7cb
fathug <fathug@users.noreply.github.com>
2024-05-13 17:08:26 +0800

    正常开启设备，取流，触发

---
+++ DahengSdkDev2/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace DahengSdkDev2
+{
+    partial class GxGetImage
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.m_pic_ShowImage = new System.Windows.Forms.PictureBox();
+            this.m_btn_OpenDevice = new System.Windows.Forms.Button();
+            this.m_btn_CloseDevice = new System.Windows.Forms.Button();
+            this.m_btn_StartDevice = new System.Windows.Forms.Button();
+            this.m_btn_StopDevice = new System.Windows.Forms.Button();
+            this.m_btn_SoftTriggerCommand = new System.Windows.Forms.Button();
+            this.m_txt_TimeOut = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.m_pic_ShowImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // m_pic_ShowImage
+            // 
+            this.m_pic_ShowImage.Location = new System.Drawing.Point(12, 63);
+            this.m_pic_ShowImage.Name = "m_pic_ShowImage";
+            this.m_pic_ShowImage.Size = new System.Drawing.Size(363, 375);
+            this.m_pic_ShowImage.TabIndex = 0;
+            this.m_pic_ShowImage.TabStop = false;
+            // 
+            // m_btn_OpenDevice
+            // 
+            this.m_btn_OpenDevice.Location = new System.Drawing.Point(445, 63);
+            this.m_btn_OpenDevice.Name = "m_btn_OpenDevice";
+            this.m_btn_OpenDevice.Size = new System.Drawing.Size(90, 40);
+            this.m_btn_OpenDevice.TabIndex = 1;
+            this.m_btn_OpenDevice.Text = "打开设备";
+            this.m_btn_OpenDevice.UseVisualStyleBackColor = true;
+            this.m_btn_OpenDevice.Click += new System.EventHandler(this.m_btn_OpenDevice_Click);
+            // 
+            // m_btn_CloseDevice
+            // 
+            this.m_btn_CloseDevice.Location = new System.Drawing.Point(608, 63);
+            this.m_btn_CloseDevice.Name = "m_btn_CloseDevice";
+            this.m_btn_CloseDevice.Size = new System.Drawing.Size(90, 40);
+            this.m_btn_CloseDevice.TabIndex = 2;
+            this.m_btn_CloseDevice.Text = "关闭设备";
+            this.m_btn_CloseDevice.UseVisualStyleBackColor = true;
+            // 
+            // m_btn_StartDevice
+            // 
+            this.m_btn_StartDevice.Location = new System.Drawing.Point(445, 129);
+            this.m_btn_StartDevice.Name = "m_btn_StartDevice";
+            this.m_btn_StartDevice.Size = new System.Drawing.Size(90, 40);
+            this.m_btn_StartDevice.TabIndex = 3;
+            this.m_btn_StartDevice.Text = "开始采集";
+            this.m_btn_StartDevice.UseVisualStyleBackColor = true;
+            this.m_btn_StartDevice.Click += new System.EventHandler(this.m_btn_StartDevice_Click);
+            // 
+            // m_btn_StopDevice
+            // 
+            this.m_btn_StopDevice.Location = new System.Drawing.Point(608, 129);
+            this.m_btn_StopDevice.Name = "m_btn_StopDevice";
+            this.m_btn_StopDevice.Size = new System.Drawing.Size(90, 40);
+            this.m_btn_StopDevice.TabIndex = 4;
+            this.m_btn_StopDevice.Text = "停止采集";
+            this.m_btn_StopDevice.UseVisualStyleBackColor = true;
+            // 
+            // m_btn_SoftTriggerCommand
+            // 
+            this.m_btn_SoftTriggerCommand.Location = new System.Drawing.Point(445, 268);
+            this.m_btn_SoftTriggerCommand.Name = "m_btn_SoftTriggerCommand";
+            this.m_btn_SoftTriggerCommand.Size = new System.Drawing.Size(100, 40);
+            this.m_btn_SoftTriggerCommand.TabIndex = 5;
+            this.m_btn_SoftTriggerCommand.Text = "发送软触发命令采图";
+            this.m_btn_SoftTriggerCommand.UseVisualStyleBackColor = true;
+            this.m_btn_SoftTriggerCommand.Click += new System.EventHandler(this.m_btn_SoftTriggerCommand_Click);
+            // 
+            // m_txt_TimeOut
+            // 
+            this.m_txt_TimeOut.Location = new System.Drawing.Point(445, 223);
+            this.m_txt_TimeOut.Name = "m_txt_TimeOut";
+            this.m_txt_TimeOut.Size = new System.Drawing.Size(100, 25);
+            this.m_txt_TimeOut.TabIndex = 6;
+            this.m_txt_TimeOut.Text = "500";
+            // 
+            // GxGetImage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.m_txt_TimeOut);
+            this.Controls.Add(this.m_btn_SoftTriggerCommand);
+            this.Controls.Add(this.m_btn_StopDevice);
+            this.Controls.Add(this.m_btn_StartDevice);
+            this.Controls.Add(this.m_btn_CloseDevice);
+            this.Controls.Add(this.m_btn_OpenDevice);
+            this.Controls.Add(this.m_pic_ShowImage);
+            this.Name = "GxGetImage";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.GxGetImage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.m_pic_ShowImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox m_pic_ShowImage;
+        private System.Windows.Forms.Button m_btn_OpenDevice;
+        private System.Windows.Forms.Button m_btn_CloseDevice;
+        private System.Windows.Forms.Button m_btn_StartDevice;
+        private System.Windows.Forms.Button m_btn_StopDevice;
+        private System.Windows.Forms.Button m_btn_SoftTriggerCommand;
+        private System.Windows.Forms.TextBox m_txt_TimeOut;
+    }
+}
+
---
+++ DahengSdkDev2/Form1.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GxIAPINET;
+using GxIAPINET.Sample.Common;
+
+namespace DahengSdkDev2
+{
+    public partial class GxGetImage : Form
+    {
+        bool m_bIsOpen = false;                  ///<设备打开状态
+        bool m_bIsSnap = false;                  ///<发送开采命令标识
+        IGXFactory m_objIGXFactory = null;                   ///<Factory对像
+        IGXDevice m_objIGXDevice = null;                   ///<设备对像
+        IGXStream m_objIGXStream = null;                   ///<流对像
+        IGXFeatureControl m_objIGXFeatureControl = null;                   ///<远端设备属性控制器对像
+        IGXFeatureControl m_objIGXStreamFeatureControl = null;                  ///<流层属性控制器对象
+        CStatistics m_objStatistic = new CStatistics();      ///<数据统计类对象用于处理统计时间
+        CStopWatch m_objStopTime = new CStopWatch();       ///<定义时间差类对象
+        GxBitmap m_objGxBitmap = null;                   ///<图像显示类对象
+
+
+
+        public GxGetImage()
+        {
+            InitializeComponent();
+        }
+
+        private void GxGetImage_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                //刷新界面
+                __UpdateUI();
+
+                m_objIGXFactory = IGXFactory.GetInstance();
+                m_objIGXFactory.Init();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// 开启设备
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void m_btn_OpenDevice_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<IGXDeviceInfo> listGXDeviceInfo = new List<IGXDeviceInfo>();
+
+                //关闭流
+                __CloseStream();
+                // 如果设备已经打开则关闭，保证相机在初始化出错情况下能再次打开
+                __CloseDevice();
+
+                m_objIGXFactory.UpdateAllDeviceList(200, listGXDeviceInfo);
+
+                // 判断当前连接设备个数
+                if (listGXDeviceInfo.Count <= 0)
+                {
+                    MessageBox.Show("未发现设备!");
+                    return;
+                }
+
+                //打开列表第一个设备
+
+                m_objIGXDevice = m_objIGXFactory.OpenDeviceBySN(listGXDeviceInfo[0].GetSN(), GX_ACCESS_MODE.GX_ACCESS_EXCLUSIVE);
+                m_objIGXFeatureControl = m_objIGXDevice.GetRemoteFeatureControl();
+
+
+                //打开流
+                if (null != m_objIGXDevice)
+                {
+                    m_objIGXStream = m_objIGXDevice.OpenStream(0);
+                    m_objIGXStreamFeatureControl = m_objIGXStream.GetFeatureControl();
+                }
+
+                // 建议用户在打开网络相机之后，根据当前网络环境设置相机的流通道包长值，
+                // 以提高网络相机的采集性能,设置方法参考以下代码。
+                GX_DEVICE_CLASS_LIST objDeviceClass = m_objIGXDevice.GetDeviceInfo().GetDeviceClass();
+                if (GX_DEVICE_CLASS_LIST.GX_DEVICE_CLASS_GEV == objDeviceClass)
+                {
+                    // 判断设备是否支持流通道数据包功能
+                    if (true == m_objIGXFeatureControl.IsImplemented("GevSCPSPacketSize"))
+                    {
+                        // 获取当前网络环境的最优包长值
+                        uint nPacketSize = m_objIGXStream.GetOptimalPacketSize();
+                        // 将最优包长值设置为当前设备的流通道包长值
+                        m_objIGXFeatureControl.GetIntFeature("GevSCPSPacketSize").SetValue(nPacketSize);
+                    }
+                }
+
+                __InitDevice();
+
+                if (null != m_objGxBitmap)
+                {
+                    m_objGxBitmap.ReleaseBuffer();
+                }
+                m_objGxBitmap = new GxBitmap(m_objIGXDevice, m_pic_ShowImage, m_objIGXStream, m_objIGXFactory);
+
+                // 更新设备打开标识
+                m_bIsOpen = true;
+
+                //刷新界面
+                __UpdateUI();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// 开启采集
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void m_btn_StartDevice_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (null != m_objIGXStreamFeatureControl)
+                {
+                    try
+                    {
+                        //设置流层Buffer处理模式为OldestFirst
+                        m_objIGXStreamFeatureControl.GetEnumFeature("StreamBufferHandlingMode").SetValue("OldestFirst");
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+
+                //开启采集流通道
+                if (null != m_objIGXStream)
+                {
+                    m_objIGXStream.StartGrab();
+                }
+
+                //发送开采命令
+                if (null != m_objIGXFeatureControl)
+                {
+                    m_objIGXFeatureControl.GetCommandFeature("AcquisitionStart").Execute();
+                }
+                m_bIsSnap = true;
+
+                // 更新界面UI
+                __UpdateUI();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// 开启一次软触发
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void m_btn_SoftTriggerCommand_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IImageData objIImageData = null;
+                double dElapsedtime = 0;
+                uint nTimeout = 500;
+
+                if (!string.IsNullOrEmpty(m_txt_TimeOut.Text))
+                {
+                    try
+                    {
+                        nTimeout = Convert.ToUInt32(m_txt_TimeOut.Text);
+                    }
+                    catch (Exception)
+                    {
+                        m_txt_TimeOut.Text = (500).ToString();
+                        MessageBox.Show("请输入正确的有效数字！");
+                        return;
+                    }
+                }
+
+                m_txt_TimeOut.Text = nTimeout.ToString();
+
+                //每次发送触发命令之前清空采集输出队列
+                //防止库内部缓存帧，造成本次GXGetImage得到的图像是上次发送触发得到的图
+                if (null != m_objIGXStream)
+                {
+                    m_objIGXStream.FlushQueue();
+                }
+
+                //发送软触发命令
+                if (null != m_objIGXFeatureControl)
+                {
+                    m_objIGXFeatureControl.GetCommandFeature("TriggerSoftware").Execute();
+                }
+
+                //获取图像
+                if (null != m_objIGXStream)
+                {
+                    //计时开始
+                    m_objStopTime.Start();
+
+                    objIImageData = m_objIGXStream.GetImage(nTimeout);
+
+                    //结束计时
+                    dElapsedtime = m_objStopTime.Stop();
+                }
+
+                m_objGxBitmap.Show(objIImageData);
+
+                if (null != objIImageData)
+                {
+                    //用完之后释放资源
+                    objIImageData.Destroy();
+                }
+
+                //更新界面的时间统计数据
+                __UpdateStatisticalData(dElapsedtime);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+
+        /// <summary>
+        /// 相机初始化
+        /// </summary>
+        private void __InitDevice()
+        {
+            if (null != m_objIGXFeatureControl)
+            {
+                //设置采集模式连续采集
+                m_objIGXFeatureControl.GetEnumFeature("AcquisitionMode").SetValue("Continuous");
+
+                //设置触发模式为开
+                m_objIGXFeatureControl.GetEnumFeature("TriggerMode").SetValue("On");
+
+                //选择触发源为软触发
+                m_objIGXFeatureControl.GetEnumFeature("TriggerSource").SetValue("Software");
+            }
+        }
+
+        /// <summary>
+        /// 关闭流
+        /// </summary>
+        private void __CloseStream()
+        {
+            try
+            {
+                //关闭流
+                if (null != m_objIGXStream)
+                {
+                    m_objIGXStream.Close();
+                    m_objIGXStream = null;
+                    m_objIGXStreamFeatureControl = null;
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        /// <summary>
+        /// 关闭设备
+        /// </summary>
+        private void __CloseDevice()
+        {
+            try
+            {
+                //关闭设备
+                if (null != m_objIGXDevice)
+                {
+                    m_objIGXDevice.Close();
+                    m_objIGXDevice = null;
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        /// <summary>
+        /// 更新界面按钮状态，是否可点击
+        /// </summary>
+        private void __UpdateUI()
+        {
+            m_btn_OpenDevice.Enabled = !m_bIsOpen;
+            m_btn_CloseDevice.Enabled = m_bIsOpen;
+            m_btn_StartDevice.Enabled = m_bIsOpen && !m_bIsSnap;
+            m_btn_StopDevice.Enabled = m_bIsSnap;
+            m_btn_SoftTriggerCommand.Enabled = m_bIsOpen && m_bIsSnap;  //开启设备且开启采集的时候才enable软触发按钮
+            //m_txt_TimeOut.Enabled = m_bIsOpen && m_bIsSnap;
+        }
+
+        /// <summary>
+        /// 更新界面的统计数据
+        /// </summary>
+        /// <param name="dData">时间间隔</param>
+        private void __UpdateStatisticalData(double dData)
+        {
+            m_objStatistic.AddStatisticalData(dData);
+
+            //// 获取平均值并显示
+            //m_txt_AveTime.Text = m_objStatistic.GetAverage().ToString("F3");
+
+            //// 获取最大值并显示
+            //m_txt_MaxTime.Text = m_objStatistic.GetMax().ToString("F3");
+
+            //// 获取最小值并显示
+            //m_txt_MinTime.Text = m_objStatistic.GetMin().ToString("F3");
+        }
+    }
+}
